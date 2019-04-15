696eee9ae24a307387dfba216dd448ebddd2c779
Sockramx <Sockramx@gmail.com>
2019-04-15 09:58:12 -0500

    Create model Lvideos

--- link/lvideos/models.py
+++ link/lvideos/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Lvideos(models.Model):
+    name = models.CharField(max_length=50)
+    link = models.CharField(max_length=300)
+    description = models.CharField(max_length=180)
+
+    def __str__(self):
+        return self.name
