2985b3b3fa1e6115454adc83a87b2d6262058fd5
Sockramx <Sockramx@gmail.com>
2019-04-15 14:38:53 -0500

    admin.py modified

--- link/lvideos/admin.py
+++ link/lvideos/admin.py
@@ -1,7 +1,25 @@
 from django.contrib import admin
-from .models import Video, Category
+from .models import Video, Category, SubCategory
+
+class VideoModelAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'link', 'description', 'source', 'user', 'category']
+
+    class Meta:
+        model = Video
+
+class CategoryModelAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'description', 'subCategory']
+
+    class Meta:
+        model = Category
+
+class SubCategoryModelAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+
+    class Meta:
+        model = SubCategory
 
 
-
-admin.site.register(Video)
-admin.site.register(Category)
+admin.site.register(Video, VideoModelAdmin)
+admin.site.register(Category, CategoryModelAdmin)
+admin.site.register(SubCategory, SubCategoryModelAdmin)
--- link/lvideos/models.py
+++ link/lvideos/models.py
@@ -1,21 +1,33 @@
 from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
-class Video(models.Model):
-    name = models.CharField(max_length=50)
-    link = models.CharField(max_length=300)
-    description = models.CharField(max_length=180)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
+
+class SubCategory(models.Model):
+    name = models.CharField(max_length=50, blank=True , null=True)
+    
     def __str__(self):
         return self.name
 
 
 class Category(models.Model):
     name = models.CharField(max_length=50)
-    description = models.CharField(max_length=50)
-    video = models.ForeignKey(Video, on_delete=models.CASCADE)
-    
+    description = models.CharField(max_length=50, blank=True, null=True)
+    subCategory = models.ForeignKey(SubCategory, on_delete=models.CASCADE, blank=True, null=True)
+
     def __str__(self):
         return self.name
+
+
+class Video(models.Model):
+    name = models.CharField(max_length=50, blank=False, null=False)
+    link = models.CharField(max_length=300, blank=True, null=True)
+    description = models.CharField(max_length=180, blank=True, null=True)
+    source = models.CharField(max_length=50, blank=True, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
