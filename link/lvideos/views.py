be763191e47b0ff0fa7c3e75a7a91a288e1701f7
Sockramx <Sockramx@gmail.com>
2019-04-15 15:34:54 -0500

    login_required

--- link/lvideos/views.py
+++ link/lvideos/views.py
@@ -1,10 +1,13 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 
-
+@login_required(login_url='login')
 def home(request):
     return render(request, 'home.html')
 
+
+@login_required(login_url='login')
 def videos(request):
     return render(request, 'lvideos/list.html')
 
